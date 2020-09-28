b04888bda38c751622c2f6ea59439f9a49009601
n. tong <natalie.227276@nv.ccsd.net>
2020-09-28 06:44:56 +0000

    completed HowFarAway

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,24 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        Scanner in = new Scanner(System.in);
+        double lumberCost = 8;
+        double windowCost = 11;
+        double taxRate, total;
+        int boards, windows;
+
+        System.out.println("Enter the sales tax rate: ");
+        taxRate = in.nextDouble();
+        System.out.println("How many boards do you need? ");
+        boards = in.nextInt();
+        System.out.println("How many windows do you need? ");
+        windows = in.nextInt();
+
+        Construction gertrude = new Construction(lumberCost, windowCost, taxRate);
+
+        total = gertrude.lumberCost(boards) + gertrude.windowCost(windows);
+        System.out.println("Total: " + total);
+
+        System.out.println("Grand Total: " + gertrude.grandTotal(total));
     }
 }
